936cf831eb533bea74ce1685e51df4760883ed09
davidscottconrad <david.scott.conrad@gmail.com>
2025-02-24 23:47:39 -0500

    removing boilderplate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,17 +8,14 @@ defineProps({
 </script>
 
 <template>
-  <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vite.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
-    </h3>
-  </div>
+  <div></div>
 </template>
 
 <style scoped>
+body {
+  background-color: #ffffffbf;
+}
+
 h1 {
   font-weight: 500;
   font-size: 2.6rem;
